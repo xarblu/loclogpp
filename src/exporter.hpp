cfedcec3b14ed5a92b7e7e46f097f8fc4fc7804d
Xarblu <xarblu@protonmail.com>
2026-06-16 23:44:20 +0200

    exporter: add note
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/exporter.hpp
+++ src/exporter.hpp
@@ -11,6 +11,9 @@ public:
     /**
      * Export DB to GPX compatible format
      * (as exported from Dawarich https://github.com/Freika/dawarich)
+     *
+     * XXX: maybe just write to STDOUT instead of parsing everything into a string?
+     *      for *very* large DBs this might be a memory issue
      */
     static std::string toGPX(Database *db);
 };
